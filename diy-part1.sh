a183af90812cf9c99cc10335f6b6eb78e2d32aaa
jinglingxiaoguin <58650263+jinglingxiaoguin@users.noreply.github.com>
2023-01-30 11:05:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git kenzo https://aithub.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git helloworld https://aithub.com/fw876/helloworld.git' >>feeds.conf.default
